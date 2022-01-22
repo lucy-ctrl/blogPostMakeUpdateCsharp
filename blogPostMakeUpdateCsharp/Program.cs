dc851f5e37f60d4b93b40269eadb3d65adab6d4f
lucy-ctrl <53624811+lucy-ctrl@users.noreply.github.com>
2022-01-22 16:28:55 +0000

    can successfully added a json object to file

--- blogPostMakeUpdateCsharp/Program.cs
+++ blogPostMakeUpdateCsharp/Program.cs
@@ -1,35 +1,80 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 
+
+
+
 namespace blogPostMakeUpdateCsharp
 {
+
     class Program
     {
-        static void Main(string[] args)
-        {
-            Post blogPost = new Post();
-            blogPost.setDateTime();
-            blogPost.setFileName();
-            Console.WriteLine("enter title for this post: \n");
-            string title = Console.ReadLine();
-            blogPost.setTitle(title);
-            Console.WriteLine("enter body of this post: \n");
-            string body = Console.ReadLine();
-            blogPost.setBody(body);
 
+
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to perform an action to do with blog posts(b) or project posts(p)");
+            string userChoice = Console.ReadLine();
+            string lowerCaseChoice = userChoice.ToLower();
+            makeChoice(lowerCaseChoice);
+        }
             
 
+             static void makeChoice(string choice)
+            {
 
-            string fileName = blogPost.getFileName();
+                switch (choice)
+                {
+                    case "b":
+                        createNewBlogPost();
+                        break;
 
-            string jsonString = JsonSerializer.Serialize(blogPost);
+                    case "p":
+                        createNewProjectPost();
+                        break;
 
-            File.WriteAllText(fileName, jsonString);
+                    default:
+                        Console.WriteLine("Invalid charachter.");
+                        break;
 
-            Console.WriteLine(File.ReadAllText(fileName));
+
+                }
+            }
 
 
+            static void createNewBlogPost()
+            {
+                Console.WriteLine("enter title for this post: \n");
+                string title = Console.ReadLine();
+                Console.WriteLine("enter body of this post: \n");
+                string body = Console.ReadLine();
+
+                var blogP = new Post
+                {
+                    dateTime = DateTime.Now,
+                    fileName = "C:/Users/hlucy/Documents/blogPostMakeUpdateCsharp/blogPostMakeUpdateCsharp/json1.json",
+                    title = title,
+                    body = body
+                };
+
+                string jsonString = JsonSerializer.Serialize(blogP);
+                //converts value in the blogP object into a json object
+
+                File.WriteAllText(blogP.fileName, jsonString);
+                //writes the newly created json object to the json file provided
+
+                Console.WriteLine(File.ReadAllText(blogP.fileName));
+                //displays content of the file
+
+            }
+
+            static void createNewProjectPost()
+            {
+                Console.WriteLine("Under development");
+            }
         }
     }
-}
+
+

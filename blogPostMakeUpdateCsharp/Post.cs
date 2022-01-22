7f5250367541cc94569fb35b98cda8e99e22598d
lucy-ctrl <53624811+lucy-ctrl@users.noreply.github.com>
2022-01-22 15:13:18 +0000

    Add project files.

---
+++ blogPostMakeUpdateCsharp/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace blogPostMakeUpdateCsharp
+{
+    class Post
+    {
+		private DateTime dateTime;
+		private string title;
+		private string body;
+		private string fileName;
+		/// <summary>
+		/// ///////////////
+		/// </summary>
+		public void setDateTime() { dateTime = DateTime.Now; }
+		public void setTitle(string t) { title = t; }
+		public void setBody(string b) { body = b; }
+		public void setFileName() { fileName = "C:/Users/hlucy/Documents/blogPostMakeUpdateCsharp/blogPostMakeUpdateCsharp/json1.json"; }
+		public string getTitle() { return title; }
+		public DateTime getDateTime() { return dateTime; }
+		public string getbody() { return body; }
+		public string getFileName() { return fileName; }
+		public void editExistingPost(string fileName, string title)
+        {
+
+        }
+		
+	}
+}
---
+++ blogPostMakeUpdateCsharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace blogPostMakeUpdateCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post blogPost = new Post();
+            blogPost.setDateTime();
+            blogPost.setFileName();
+            Console.WriteLine("enter title for this post: \n");
+            string title = Console.ReadLine();
+            blogPost.setTitle(title);
+            Console.WriteLine("enter body of this post: \n");
+            string body = Console.ReadLine();
+            blogPost.setBody(body);
+
+            
+
+
+            string fileName = blogPost.getFileName();
+
+            string jsonString = JsonSerializer.Serialize(blogPost);
+
+            File.WriteAllText(fileName, jsonString);
+
+            Console.WriteLine(File.ReadAllText(fileName));
+
+
+        }
+    }
+}
